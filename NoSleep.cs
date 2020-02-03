135a15ddbe678a83b9d8222a60077432b7aba696
Gamer_Z <github@gz0.nl>
2020-02-03 01:01:27 +0100

    Initial commit

---
+++ NoSleep.cs
@@ -0,0 +1,20 @@
+﻿using static ModLoader;
+using static ServerManager;
+
+namespace grasmanek94.NoSleep
+{
+    [ModManager]
+    public static class NoSleep
+    {
+        [ModCallback(EModCallbackType.AfterWorldLoad, "AfterWorldLoad")]
+        static void AfterWorldLoad()
+        {
+            ServerSettings.Time.SleepTimeStart = 23.999f;
+            ServerSettings.Time.SleepTimeEnd = 0.000f;
+            ServerSettings.Time.GuardShiftDayStart = ServerManager.ServerSettings.Time.SleepTimeEnd;
+            ServerSettings.Time.GuardShiftDayEnd = ServerManager.ServerSettings.Time.SleepTimeStart;
+            ServerSettings.Time.GuardShiftNightStart = ServerManager.ServerSettings.Time.SleepTimeEnd;
+            ServerSettings.Time.GuardShiftNightEnd = ServerManager.ServerSettings.Time.SleepTimeStart;
+        }
+    }
+}
